4199b1a4165491531d27b87947789ec5b6150161
Alex Pliutau <a.pliutau@gmail.com>
2017-10-10 14:17:19 +0700

    Check if not windows

--- symbols.go
+++ symbols.go
@@ -1,5 +1,7 @@
 package wow
 
+import "runtime"
+
 // LogSymbol is a log severity level
 type LogSymbol uint
 
@@ -12,16 +14,28 @@ const (
 )
 
 func (s LogSymbol) String() string {
-	switch s {
-	case INFO:
-		return "ℹ"
-	case SUCCESS:
-		return "✔"
-	case WARNING:
-		return "⚠"
-	case ERROR:
-		return "✖"
-	default:
-		return ""
+	if runtime.GOOS == "windows" {
+		switch s {
+		case INFO:
+			return "ℹ"
+		case SUCCESS:
+			return "✔"
+		case WARNING:
+			return "⚠"
+		case ERROR:
+			return "✖"
+		}
+	} else {
+		switch s {
+		case INFO:
+			return "i"
+		case SUCCESS:
+			return "v"
+		case WARNING:
+			return "!!"
+		case ERROR:
+			return "x"
+		}
 	}
+	return ""
 }
